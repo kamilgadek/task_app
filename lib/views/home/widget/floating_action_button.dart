c42f6e8a7335e7e111866fef5686cfce76abbae0
Kamil Gadek <kamil.gadek124@gmail.com>
2024-05-07 20:29:18 +0200

    Creating FloatingActionButton

--- lib/views/home/widget/floating_action_button.dart
+++ lib/views/home/widget/floating_action_button.dart
@@ -1,24 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:task_app/utils/app_colors.dart';
-
-class HomeView extends StatefulWidget {
-  const HomeView({super.key});
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      floatingActionButton: FloatingActionButton(),
-    );
-  }
-}
 
 class FloatingActionButton extends StatelessWidget {
   const FloatingActionButton({
