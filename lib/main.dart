13440ef70d93d6854b9f9252a73574298f87466d
Kamil Gadek <kamil.gadek124@gmail.com>
2024-05-07 20:15:21 +0200

    Add font style and colors to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/views/home/home_view.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Hive ToDo App',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Colors.black,
+            fontSize: 45,
+            fontWeight: FontWeight.bold,
+          ),
+          titleMedium: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+            fontWeight: FontWeight.w300,
+        ),
+       displayMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 21,
+        ),
+        displaySmall: TextStyle(
+            color: Color.fromARGB(255, 234, 234, 234),
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+        ),
+        headlineMedium: TextStyle(
+            color: Colors.grey,
+            fontSize: 17,
+        ),
+        headlineSmall: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+        ),
+        titleSmall: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+        ),
+        titleLarge: TextStyle(
+            color: Colors.black,
+            fontSize: 40,
+            fontWeight: FontWeight.w300
         ),
       ),
+      ),
+      home: HomeView(),
     );
   }
 }
+
+
+class TestPage extends StatefulWidget {
+  const TestPage({super.key});
+
+  @override
+  State<TestPage> createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
+
