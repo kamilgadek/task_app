99804b6d83a58b2626064406312f33baab2d95bb
Kamil Gadek <kamil.gadek124@gmail.com>
2024-05-09 20:38:47 +0200

    Create a scorlable task list

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/space_exts.dart';
 import 'package:task_app/utils/app_colors.dart';
+import 'package:task_app/utils/app_str.dart';
 import 'widget/floating_action_button.dart';
 
 class HomeView extends StatefulWidget {
@@ -12,6 +14,8 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       backgroundColor: Colors.white,
       floatingActionButton: const Fab(),
@@ -20,15 +24,16 @@ class _HomeViewState extends State<HomeView> {
         width: double.infinity,
         child: Column(
           children: [
+            // Custome AppBar
             Container(
               margin: const EdgeInsets.only(top: 60),
               width: double.infinity,
               height: 100,
-              color: Colors.red,
-              child: const Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(
+                  // Circullar Progress Indicator (kółko)
+                  const SizedBox(
                     height: 30,
                     width: 30,
                     child: CircularProgressIndicator(
@@ -39,9 +44,61 @@ class _HomeViewState extends State<HomeView> {
                       ),
                     ),
                   ),
+                  25.w,
+
+                  // Top level task info (1 of 3)
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        AppStr.mainTitle,
+                        style: textTheme.displayLarge,
+                      ),
+                      3.h,
+                      Text(
+                        '1 of 3 task',
+                        style: textTheme.titleMedium,
+                      )
+                    ],
+                  ),
                 ],
               ),
             ),
+
+            // Divider (linia pod AppBar)
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Divider(
+                thickness: 2,
+                indent: 100,
+              ),
+            ),
+
+            //Tasks List
+            SizedBox(
+              height: 600,
+              width: double.infinity,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return AnimatedContainer(
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                    decoration: BoxDecoration(
+                      color: AppColors.primaryColor.withOpacity(0.3),
+                    ),
+                    duration: const Duration(
+                      microseconds: 600,
+                    ),
+                    child: const ListTile(),
+                  );
+                },
+              ),
+            )
           ],
         ),
       ),
