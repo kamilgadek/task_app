9dd3f7f69ee75dd8a3bc89edd0993791667ab387
Kamil Gadek <kamil.gadek124@gmail.com>
2024-05-20 18:41:29 +0200

    Add title in TaskView

--- lib/views/tasks/task_view.dart
+++ lib/views/tasks/task_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/utils/app_str.dart';
 import 'package:task_app/views/tasks/widget/task_view_app_bar.dart';
 
 class TaskView extends StatefulWidget {
@@ -11,8 +12,52 @@ class TaskView extends StatefulWidget {
 class _TaskViewState extends State<TaskView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: TaskViewAppBar(),
+    var textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      // appBar
+      appBar: const TaskViewAppBar(),
+
+      // Body
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+                width: double.infinity,
+                height: 100,
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 70,
+                      child: Divider(
+                        thickness: 2,
+                      ),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: AppStr.addNewTask + ' ',
+                        style: textTheme.titleLarge,
+                        children: const [
+                          TextSpan(
+                            text: AppStr.taskString,
+                            style: TextStyle(fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 70,
+                      child: Divider(
+                        thickness: 2,
+                      ),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
